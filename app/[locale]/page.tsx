fbfad29bfd4512316773d2d3c74a30aacd717ccc
Artades <artemijgalaj@gmail.com>
2024-08-18 22:15:53 +0500

    some changes

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,35 +1,7 @@
-import Image from "next/image";
-import styles from "../page.module.css"
-import { useTranslations } from "next-intl";
+import Intro from "@/components/Intro/Intro";
 
-export default function Home() {
-  const t = useTranslations("Home");
+const Page = () => {
+	return <Intro />;
+};
 
-  // Extract the navigation object keys from the translations
-  const navigationKeys = Object.keys(t.raw("navigation"));
-  return (
-    <main className={styles.main}>
-        <>
-      <nav>
-        <ul>
-          {navigationKeys.map((key) => (
-            <li key={key}>
-              <a href={`#/${key}`}>{t(`navigation.${key}`)}</a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <main>
-        <div>
-          <aside>
-            <h2>{t("title")}</h2>
-            <p dangerouslySetInnerHTML={{ __html: t("description") }}></p>
-          </aside>
-        
-        </div>
-      </main>
-    </>
-
-    </main>
-  );
-}
+export default Page;
