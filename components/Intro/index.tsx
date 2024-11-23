10d45a4d1b2201a7378a07405a0ae1617d9c3703
Artyom Galay <artemijgalaj@gmail.com>
2024-11-23 22:36:56 +0500

    basic styles and Intro component

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -1,28 +1,81 @@
+"use client";
 import { EButtonSizes } from "@/types/ui";
-import StarGrid from "../AnimatedGrid/StarGrid";
 import Button from "../ui/Button/Button";
 import styles from "./styles.module.scss";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import usePrefersReducedMotion from "@/hooks/ui/usePrefersReducedMotion";
 
 export default function Intro() {
-	return (
-		<div className={styles.intro}>
-			{/* <StarGrid /> */}
-			<div className={styles.introContent}>
-				<h1 className={styles.introHeading}>
-					<span>Innovate.</span>
+  const headingRef = useRef(null);
+  const textRef = useRef(null);
+  const buttonRef = useRef(null);
+  const creator = useRef(null);
+  const container = useRef(null);
 
-					<span>Create.</span>
+  const prefersReducedMotion = usePrefersReducedMotion();
 
-					<span>Inspire</span>
-				</h1>
-				<p className={styles.introText}>
-					Hi, I’m Artyom Galay, a developer focused on building innovative,
-					user-driven websites and applications that merge creativity with
-					functionality.
-				</p>
+  useGSAP(
+    () => {
+      if (prefersReducedMotion) {
+        gsap.set(container.current, { opacity: 1 });
+        gsap.set(headingRef.current?.children, { opacity: 1, y: 0 });
+        gsap.set(textRef.current, { opacity: 1, y: 0 });
+        gsap.set(buttonRef.current, { opacity: 1, y: 0 });
+        return;
+      }
 
-				<Button text="Resumee" size={EButtonSizes.DEFAULT}  />
-			</div>
-		</div>
-	);
+      gsap.set(headingRef.current?.children, { opacity: 0, y: 50 });
+      gsap.set(textRef.current, { opacity: 0, y: 30 });
+      gsap.set(buttonRef.current, { opacity: 0, y: 20 });
+      gsap.set(container.current, { opacity: 1 });
+      gsap.set(creator.current, { color: "#fff" });
+
+      const tl = gsap.timeline();
+
+      tl.to(headingRef.current?.children, {
+        opacity: 1,
+        y: 0,
+        stagger: 0.2,
+        duration: 1,
+      });
+
+      tl.to(textRef.current, {
+        opacity: 1,
+        y: 0,
+        duration: 0.6,
+      });
+      
+      tl.to(buttonRef.current, {
+        opacity: 1,
+        y: 0,
+        duration: 0.4,
+      });
+
+	  tl.to(creator.current, {
+        color: "#18aaff",
+		duration: 0.6,
+      });
+    },
+    { scope: container }
+  );
+
+  return (
+    <div className={styles.intro} ref={container}>
+      <div className={styles.introContent}>
+        <h1 className={styles.introHeading} ref={headingRef}>
+          <span>Innovate.</span>
+          <span>Create.</span>
+          <span>Inspire.</span>
+        </h1>
+        <p className={styles.introText} ref={textRef}>
+          Hi, I’m <span ref={creator}>Artyom Galay</span>, a developer focused
+          on building innovative, user-driven websites and applications that
+          merge creativity with functionality.
+        </p>
+        <Button text="Resumee" size={EButtonSizes.DEFAULT} ref={buttonRef} />
+      </div>
+    </div>
+  );
 }
